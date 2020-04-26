44f8eb532f4744c9f2f1dbbb01ca24c5e1608e91
JavierR <frjaraur@gmail.com>
2020-04-26 18:36:26 +0200

    Chapter 1

---
+++ environments/kubernetes-environment/labs/create-docker-database.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=0 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE USER docker;
+    CREATE DATABASE docker;
+    GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
+EOSQL
+
